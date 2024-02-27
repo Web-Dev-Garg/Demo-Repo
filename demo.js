3d04996a5871f991b5bbabf17a0559ebf7ff294a
Web-Dev-Garg <161446737+Web-Dev-Garg@users.noreply.github.com>
2024-02-27 15:14:43 +0530

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,14 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+})
+
+app.listen(port, ()=>{
+    console.log(`App is listening to the port ${port}`);
+})
